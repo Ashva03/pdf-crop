5fac210f4068908c93d6ea94640cd17f2e485e08
ANIKET1205 <aniketramani1205@gmail.com>
2025-04-06 19:50:16 +0530

    Fix build error

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
             config.resolve.fallback = {
                 ...config.resolve.fallback,
                 fs: false,
+                canvas: false,
             };
         }
         return config;
