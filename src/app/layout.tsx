8d628bd86a944f8f3cb612b0fb25c13872f6c5ab
ANIKET1205 <aniketramani1205@gmail.com>
2026-07-18 07:21:54 +0530

    Update sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,13 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6259586123575519"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body
         className={inter.className}
